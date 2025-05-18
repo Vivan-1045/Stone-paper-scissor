3c1f6b577ebb9e25c5f24b0c09a6017664a3846f
Vivek <168336164+Vivan-1045@users.noreply.github.com>
2025-05-18 14:56:53 +0530

    Update SPS.js sound effect applied

--- Stone/public/SPS.js
+++ Stone/public/SPS.js
@@ -1,60 +1,3 @@
-// let userScore = 0;
-// let compScore = 0;
-
-// const gameimages = document.querySelectorAll(".gameimg");
-// const msg1 = document.querySelector(".msg");
-// const uScore = document.querySelector(".user-score");
-// const cScore = document.querySelector(".comp-score");
-
-// const GetcompChoice = () => {
-//     const chances = ["rock", "paper", "scissor"];
-//     const idx = Math.floor(Math.random() * 3);
-//     return chances[idx];
-// };
-
-// const shoWinner = (youWin, compChoice, userChoice) => {
-//     if (youWin) {
-//         userScore++;
-//         uScore.innerText = userScore;
-//         msg1.innerText = `You win! Your ${userChoice} beats ${compChoice}...🏆`;
-//         msg1.style.backgroundColor = "#90ee90";
-//     } else {
-//         compScore++;
-//         cScore.innerText = compScore;
-//         msg1.innerText = `You lose! ${compChoice} beats ${userChoice}...😟`;
-//         msg1.style.backgroundColor = "#ffb6b6";
-//     }
-// };
-
-// const drawgame = () => {
-//     msg1.innerText = `Game draw! Try again...📍`;
-//     msg1.style.backgroundColor = "#e0b3ff";
-// };
-
-// const gameplay = (userChoice) => {
-//     const compChoice = GetcompChoice();
-//     if (userChoice === compChoice) {
-//         drawgame();
-//     } else {
-//         let youWin = true;
-//         if (userChoice === "paper") {
-//             youWin = compChoice === "scissor" ? false : true;
-//         } else if (userChoice === "scissor") {
-//             youWin = compChoice === "rock" ? false : true;
-//         } else {
-//             youWin = compChoice === "paper" ? false : true;
-//         }
-//         shoWinner(youWin, compChoice, userChoice);
-//     }
-// };
-
-// gameimages.forEach((gameimg) => {
-//     gameimg.addEventListener("click", () => {
-//         const userChoice = gameimg.getAttribute("id");
-//         gameplay(userChoice);
-//     });
-// });
-
 let userScore = 0;
 let compScore = 0;
 
@@ -63,7 +6,6 @@ const msg1 = document.querySelector(".msg");
 const uScore = document.querySelector(".user-score");
 const cScore = document.querySelector(".comp-score");
 
-// Get audio elements
 const clickSound = document.getElementById("click-sound");
 const winSound = document.getElementById("win-sound");
 const loseSound = document.getElementById("lose-sound");
@@ -119,7 +61,6 @@ const gameplay = (userChoice) => {
 
 gameimages.forEach((gameimg) => {
     gameimg.addEventListener("click", () => {
-        // Play click sound
         clickSound.currentTime = 0;
         clickSound.play();
         const userChoice = gameimg.getAttribute("id");
